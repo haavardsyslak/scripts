73ba588167f3e357215c43ad585be43290be080d
haavardsyslak <haavard.syslak@gmail.com>
2025-08-19 14:04:21 +0200

    change docker detatch keys

--- devcon.sh
+++ devcon.sh
@@ -8,5 +8,5 @@ if [[ $dir == $HOME* ]]; then
 fi
 
 
-docker exec -it --workdir $cd_path blunux-devcontainer zsh
+docker exec -it --detach-keys="ctrl-q,ctrl-q" --workdir $cd_path blunux-devcontainer zsh
 # devcontainer exec --workspace-folder=$devcon_root bash -c "cd $cd_path && exec zsh"
